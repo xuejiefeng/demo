37654a1c22e1f9008a4e20f036fa399a1d138f30
xuejiefeng <2273394239@qq.com>
2021-07-13 21:40:46 +0800

    dw

---
+++ src/main/java/com/example/demo/HelloServlet.java
@@ -0,0 +1,33 @@
+package com.example.demo;
+
+import java.io.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+
+/**
+ * @author 薛杰锋
+ */
+@WebServlet(name = "helloServlet", value = "/hello-servlet")
+public class HelloServlet extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() {
+        message = "Hello World!";
+    }
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        response.setContentType("text/html");
+
+        // Hello
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>" + message + "</h1>");
+        out.println("</body></html>");
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
